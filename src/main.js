e9850dd7acbcf729013b51ac591bcbadf6742c80
Abderrahim Berhiddich <berhiddich@gmail.com>
2024-09-12 11:34:03 +0100

    section menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ const hamburger=document.getElementById('hamburger');
 const mobileMenu=document.getElementById('mobileMenu')
 const closenav=document.getElementById('closenav')
 const navlink=document.querySelectorAll('.navlink')
+const itmsWrap=document.querySelectorAll('.itms_wrap')
+const foods=document.querySelectorAll('.food')
+const snacks=document.querySelectorAll('.snack')
+const beverages=document.querySelectorAll('.beverage')
+
+
 
 navlink.forEach(link=>{
 link.addEventListener('click',()=>{
@@ -27,6 +33,35 @@ tabs.forEach(tab => {
     
           });
           tab.classList.add('active')
+          const tabval=tab.getAttribute('data-tabs');
+          itmsWrap.forEach(item=>{
+            item.style.display='none'
+          })
+          if(tabval=='food')
+          {
+            foods.forEach(item=>{
+                item.style.display='block'
+              })
+          }
+        else  if(tabval=='snack')
+            {
+                snacks.forEach(item=>{
+                  item.style.display='block'
+                })
+            }
+            else if(tabval=='beverage')
+                {
+                    beverages.forEach(item=>{
+                      item.style.display='block'
+                    })
+                }
+                else
+                {  itmsWrap.forEach(item=>{
+                    item.style.display='block'
+                  })
+
+                }
 
     })
 });
+
